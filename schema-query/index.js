db97404e207ced44a1be590e32cd2ef08eab243a
joaopjk <joaopjk48@gmail.com>
2025-03-15 21:37:40 -0300

    Type usuario

--- schema-query/index.js
+++ schema-query/index.js
@@ -1,17 +1,37 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
+    scalar Date
+
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entrada da sua API!
     type Query {
         hello: String!,
-        dataHoraAtual: String!
+        dataHoraAtual: Date!,
+        usuarioLogado: Usuario
     }
 `;
 const resolvers = {
     Query: {
         hello: () => 'Hello world!',
-        dataHoraAtual: () => new Date().toISOString()
-    },
+        dataHoraAtual: () => new Date,
+        usuarioLogado: () => ({
+            id: 1,
+            nome: 'Joao Sousa',
+            email: 'joao.sousa@email.com',
+            idade: 25,
+            salario: 1000.00,
+            vip: true
+        })
+    }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
