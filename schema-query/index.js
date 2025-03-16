e3a845de2d2ee8fad2543cafc2d361fc06ec9bb0
joaopjk <joaopjk48@gmail.com>
2025-03-15 21:24:42 -0300

    Desafio do retorna da data hora atual

--- schema-query/index.js
+++ schema-query/index.js
@@ -3,17 +3,18 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
     # Pontos de entrada da sua API!
     type Query {
-        hello: String!
+        hello: String!,
+        dataHoraAtual: String!
     }
 `;
 const resolvers = {
     Query: {
         hello: () => 'Hello world!',
+        dataHoraAtual: () => new Date().toISOString()
     },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
 server.listen().then(({ url }) => {
     console.log(`Server ready at ${url}`);
 });
